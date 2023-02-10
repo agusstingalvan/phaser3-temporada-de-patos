b60f9fc33dd34787efa8511fad222a0a2a08c52f
Agustin Galvan <agusstingalvan2@gmail.com>
2023-02-10 14:09:20 -0300

    add traductions in all scenes

---
+++ src/services/translations.js
@@ -0,0 +1,77 @@
+import {EN_US, ES_AR } from '../enums/languages';
+
+const PROJECT_ID = '40';
+let translations = null;
+let language = ES_AR;
+
+export async function getTranslations(lang = language, callback) {
+    localStorage.setItem('translations', '{}');
+    language = lang;
+    // if (language === ES_AR) {
+    //     return callback ? callback() : false;
+    // }
+
+    return await fetch(`https://traduci-la-strapi.herokuapp.com/api/translations/${PROJECT_ID}/${language}`)
+    .then(response => response.json())
+    .then(data => {
+        localStorage.setItem('translations', JSON.stringify(data));
+        translations = data;
+        if(callback) callback()
+    });
+}
+
+export function getPhrase(key) {
+    if (!translations) {
+        // console.log("🚀 ~ file: translations.js ~ line 25 ~ getPhrase ~ translations", translations)
+        const locals = localStorage.getItem('translations');
+        translations = locals ? JSON.parse(locals) : null;
+    }
+
+    let phrase = key;
+    if (translations && translations[key]) {
+        phrase = translations[key];
+    }
+    // console.log(phrase)
+    return phrase;
+}
+
+function isAllowedLanguge(language) {
+    const allowedLanguages = [ES_AR, EN_US];
+    return allowedLanguages.includes(language);
+}
+
+export function getLanguageConfig() {
+    let languageConfig;
+
+    // Obtener desde la URL el idioma
+    console.log(window.location.href)
+
+    /* 
+      depende como lo manejemos: 
+      1) puede venir como www.dominio.com/es-AR
+      2) puede venir como www.dominio.com?lang=es-AR
+      En el primer caso se obtiene con: window.location.pathname
+      En el segundo caso se obtiene leyendo el query param lang 
+      
+      vamos a implementar una logica que cubra ambos casos
+    */
+
+    const path = window.location.pathname;
+    const params = new URL(window.location.href).searchParams;
+    const queryLang = params.get('lang');
+
+    languageConfig = path ?? queryLang;
+
+    if (languageConfig) {
+        if (isAllowedLanguge(languageConfig)) {
+            return languageConfig;
+        }
+    }
+
+    const browserLanguage = window.navigator.language;
+    if (isAllowedLanguge(browserLanguage)) {
+        return browserLanguage;
+    }
+
+    return ES_AR;
+}
